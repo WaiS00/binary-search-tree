c2a2b92a7a9c155fb8824c6968a2f1382fcccf75
WaiS <waisiong144@gmail.com>
2022-05-26 15:51:28 +0800

    added indentation and comments to explain the codes

--- binary_search_Tree/BinarySearchTree.java
+++ binary_search_Tree/BinarySearchTree.java
@@ -27,18 +27,22 @@ public class BinarySearchTree{
 
 	//function to find specific data elements in the tree.
 	public static Node find(int data){
+		// root number
 		Node temp=root;
 		Node prev=null;
 		
 		while(temp!=null){
 			prev=temp;
 			
+			// find the number according to the sequence of the tree values
 			if(temp.key==data){
 				return prev;
 			}
+			// if the data is less than the node, search from the left node
 			if(temp.key>data){
 				temp=temp.left;
 			}
+			// if the data is more than the node, search from the right node
 			else{
 				temp=temp.right;
 			}
@@ -48,6 +52,7 @@ public class BinarySearchTree{
 
 	//function to add elements in the tree.
 	public static void add(int data){
+		// to preview the insertion of data
 		System.out.println("Inserting data : " + data);
 		Node node = new Node(data);
 		Node positionNode = find(data);
@@ -57,11 +62,12 @@ public class BinarySearchTree{
 		}
 	
 		node.parent=positionNode;
-
+		// if the new node is more than the root, it will move to the right node
 		if(node.key>positionNode.key){
 			positionNode.right=node;
 		}
 		else{
+		// if the new node is less than the root, it will move to the left node
 			positionNode.left=node;
 		}
 	}
@@ -110,10 +116,13 @@ public class BinarySearchTree{
 		}
 	}
 	
-
+	// function to remove a node from the tree
 	protected static Node remove(int data){
+		// preview of what to remove
 		System.out.println("Remove : " + data);
+		// find the node
 		Node node = find(data);
+		// if there is no node on the right side of the tree
 		if(node.right==null){
 			if(root==node){
 				root=node.left;
@@ -127,7 +136,7 @@ public class BinarySearchTree{
 			}
 			return node;
 		}
-		
+		// move the the left side of the tree
 		else{
 			Node temp = next(data);
 			node.key=temp.key;
@@ -142,6 +151,7 @@ public class BinarySearchTree{
 	}
 
 	//function to find the max data value in the tree.
+	// find the most right node in the tree
 	public static Node maxNode(){
 		if(root==null){
 			return null;
@@ -154,6 +164,7 @@ public class BinarySearchTree{
 	}
 
 	//function to find the min data value in the tree.
+	// find the most left node in the tree
 	public static Node minNode(){
 		if(root==null){
 			return null;
@@ -170,6 +181,7 @@ public class BinarySearchTree{
 		if(rootNode==null){
 			return;
 		}
+		// the formula for inorder is left, node, and right
 		inorderTraversal(rootNode.left);
 		System.out.print(rootNode.key + " ");
 		inorderTraversal(rootNode.right);
@@ -180,6 +192,7 @@ public class BinarySearchTree{
 		if(rootNode==null){
 			return;
 		}
+		// the formula for preorder is node, left, and right
 		System.out.print(rootNode.key + " " );
 		preorderTraversal(rootNode.left);
 		preorderTraversal(rootNode.right);
@@ -190,6 +203,7 @@ public class BinarySearchTree{
 		if(rootNode==null){
 			return;
 		}
+		// the formula for postorder is left, right and node
 		postorderTraversal(rootNode.left);
 		postorderTraversal(rootNode.right);
 		System.out.print(rootNode.key + " ");
--- binary_search_Tree/BinarySearchTreeImplementation.java
+++ binary_search_Tree/BinarySearchTreeImplementation.java
@@ -4,6 +4,8 @@ import java.util.Scanner;  // Import the Scanner class
 public class BinarySearchTreeImplementation extends BinarySearchTree{
 		public static void main(String [] args){
 			BinarySearchTree tree = new BinarySearchTree();
+
+			// loop to scan 10 numbers
 			for (int i = 0; i < 10; i++) {
 				Scanner myObj = new Scanner(System.in);
 				System.out.println("\nEnter Int");
@@ -14,30 +16,50 @@ public class BinarySearchTreeImplementation extends BinarySearchTree{
 			tree.display();
 	
 			System.out.println();
+
+			// method to show maximum value in the tree
 			Node maxNode = tree.maxNode();
 			System.out.println("Max value : " + maxNode.key);
+
+			// method to show minimum value in the tree
 			Node minNode = tree.minNode();
 			System.out.println("Min value : " + minNode.key);
 			
 			Scanner myObj = new Scanner(System.in);
+			
+			// Scan to find the selected value
 			System.out.println("\nEnter an integer to check does it exist: ");
 			int number = myObj.nextInt();  
+			//find number in the tree
 			Node node1 = tree.find(number);
 
 			if(number == node1.key){
-				System.out.println("The number " + node1.key + " exist");
+				// if number that is entered == the node number from the tree, it will display that the number exist in the tree
+				System.out.println("The number " + node1.key + " exist in the tree");
 			}
 			else{
-				System.out.println("The number " + number +" does not exist");
+				// if number that is entered != the node number from the tree, it will display that the number does not exist in the tree
+				System.out.println("The number " + number +" does not exist in the tree");
 			}
 
+			// scan for user input on what number to delete from tree
 			Scanner myObj1 = new Scanner(System.in);
 			System.out.println("\nEnter Int to delete from the tree");
 			int number2 = myObj1.nextInt();  
+			// find the value in the tree
+			Node del1 = tree.find(number2);
 
-			Node del1 = tree.remove(number2);
-			System.out.println(del1.key);
-			tree.display();
+			// if number that is entered is in the tree, it will remove the value and display the tree after the deletion of value
+			if(number2 == del1.key){
+				tree.remove(number2);
+				System.out.println(del1.key);
+				tree.display();
+			}
+			else{
+				// number does not exist in the tree
+				System.out.println("The number " + number2 +" does not exist in the tree");	
+			}
+
 			}
 		}
 	
