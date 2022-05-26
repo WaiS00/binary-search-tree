43d13281e3e0d5d4eac7a1919dba969364983e1f
WaiS <waisiong144@gmail.com>
2022-05-26 16:11:50 +0800

    Changes variable name for binary search tree

--- binary_search_Tree/BinarySearchTree.java
+++ binary_search_Tree/BinarySearchTree.java
@@ -1,20 +1,19 @@
-//Program to implement BinarySearchTree.
 package binary_search_tree;
 
 import java.util.Scanner;
 
 public class BinarySearchTree{
 	static class Node{
-		int key;     //data value
-		Node parent; //points to the parent node.
-		Node left;   //points to the right child.
-		Node right;  //points to the left child.
+		int keyValue;     //data value
+		Node parentNode; //points to the parent node.
+		Node leftNode;   //points to the left child.
+		Node rightNode;  //points to the right child.
 		
 		public Node(int data){
-			this.key=data;
-			this.parent=null;
-			this.left=null;
-			this.right=null;
+			this.keyValue=data;
+			this.parentNode=null;
+			this.leftNode=null;
+			this.rightNode=null;
 		}
 
 	}
@@ -26,63 +25,63 @@ public class BinarySearchTree{
 	}
 
 	//function to find specific data elements in the tree.
-	public static Node find(int data){
+	public static Node findValue(int data){
 		// root number
-		Node temp=root;
-		Node prev=null;
+		Node top=root;
+		Node prevNum=null;
 		
-		while(temp!=null){
-			prev=temp;
+		while(top!=null){
+			prevNum=top;
 			
 			// find the number according to the sequence of the tree values
-			if(temp.key==data){
-				return prev;
+			if(top.keyValue==data){
+				return prevNum;
 			}
 			// if the data is less than the node, search from the left node
-			if(temp.key>data){
-				temp=temp.left;
+			if(top.keyValue>data){
+				top=top.leftNode;
 			}
 			// if the data is more than the node, search from the right node
 			else{
-				temp=temp.right;
+				top=top.rightNode;
 			}
 		}
-		return prev;
+		return prevNum;
 	}
 
 	//function to add elements in the tree.
-	public static void add(int data){
+	public static void addValue(int data){
 		// to preview the insertion of data
 		System.out.println("Inserting data : " + data);
 		Node node = new Node(data);
-		Node positionNode = find(data);
-		if(positionNode==null){
+		Node selectedNode = findValue(data);
+		if(selectedNode==null){
 			root=node;
 			return;
 		}
 	
-		node.parent=positionNode;
+		node.parentNode=selectedNode;
 		// if the new node is more than the root, it will move to the right node
-		if(node.key>positionNode.key){
-			positionNode.right=node;
+		if(node.keyValue>selectedNode.keyValue){
+			selectedNode.rightNode=node;
 		}
 		else{
-		// if the new node is less than the root, it will move to the left node
-			positionNode.left=node;
+			// if the new node is less than the root, it will move to the left node
+			selectedNode.leftNode=node;
 		}
 	}
 
 
 	//function to find the next greater element from the given data value.
-	public static Node next(int data){
+	public static Node nextValue(int data){
 		System.out.println("Find Next of : " + data);
 		if(root==null){
 			return null;
 		}
 		
-		Node node = find(data);
-		if(node.right!=null){
-			return leftDecesdant(node.right);
+		Node node = findValue(data);
+		if(node.rightNode!=null){
+			return leftDecesdant(node.rightNode);
 		}
 		else{
 			return rightAncestor(node);
@@ -91,60 +90,60 @@ public class BinarySearchTree{
 
 	//function to find leftmost node.
 	private static Node leftDecesdant(Node node){
-		Node temp=node;
-		while(temp.left!=null){
-			temp=temp.left;
+		Node top=node;
+		while(top.leftNode!=null){
+			top=top.leftNode;
 		}
-		return temp;
+		return top;
 	}
 
 	//function to find the parent node with data value greater than the given node data value.	
 	private static Node rightAncestor(Node node){
-		Node temp=node.parent;
-		while(temp!=null){
-			if(temp.key>node.key){
+		Node top=node.parentNode;
+		while(top!=null){
+			if(top.keyValue>node.keyValue){
 				break;
 			}
-			temp=temp.parent;
+			top=top.parentNode;
 		}
-		if(temp==null){
+		if(top==null){
 			System.out.println("Max Element");
 			return node;
 		}
 		else{
-			return temp;
+			return top;
 		}
 	}
 	
 	// function to remove a node from the tree
-	protected static Node remove(int data){
+	protected static Node removeValue(int data){
 		// preview of what to remove
 		System.out.println("Remove : " + data);
 		// find the node
-		Node node = find(data);
+		Node node = findValue(data);
 		// if there is no node on the right side of the tree
-		if(node.right==null){
+		if(node.rightNode==null){
 			if(root==node){
-				root=node.left;
+				root=node.leftNode;
 				return node;
 			}
-			if(node.parent.left==node){
-				node.parent.left=node.left;
+			if(node.parentNode.leftNode==node){
+				node.parentNode.leftNode=node.leftNode;
 			}
 			else{
-				node.parent.right=node.left;
+				node.parentNode.rightNode=node.leftNode;
 			}
 			return node;
 		}
 		// move the the left side of the tree
 		else{
-			Node temp = next(data);
-			node.key=temp.key;
-			if(temp.parent.left==temp){
-				temp.parent.left=temp.right;
+			Node top = nextValue(data);
+			node.keyValue=top.keyValue;
+			if(top.parentNode.leftNode==top){
+				top.parentNode.leftNode=top.rightNode;
 			}
-			if(temp.parent.right==temp){
-				temp.parent.right=temp.right;
+			if(top.parentNode.rightNode==top){
+				top.parentNode.rightNode=top.rightNode;
 			}
 		}
 		return node;
@@ -152,28 +151,28 @@ public class BinarySearchTree{
 
 	//function to find the max data value in the tree.
 	// find the most right node in the tree
-	public static Node maxNode(){
+	public static Node maxNumber(){
 		if(root==null){
 			return null;
 		}
-		Node temp=root;
-		while(temp.right!=null){
-			temp=temp.right;
+		Node top=root;
+		while(top.rightNode!=null){
+			top=top.rightNode;
 		}
-		return temp;
+		return top;
 	}
 
 	//function to find the min data value in the tree.
 	// find the most left node in the tree
-	public static Node minNode(){
+	public static Node minNumber(){
 		if(root==null){
 			return null;
 		}
-		Node temp = root;
-		while(temp.left!=null){
-			temp=temp.left;
+		Node top = root;
+		while(top.leftNode!=null){
+			top=top.leftNode;
 		}
-		return temp;
+		return top;
 	}
 
 	//function for InOrder Traversal of the tree.
@@ -182,9 +181,9 @@ public class BinarySearchTree{
 			return;
 		}
 		// the formula for inorder is left, node, and right
-		inorderTraversal(rootNode.left);
-		System.out.print(rootNode.key + " ");
-		inorderTraversal(rootNode.right);
+		inorderTraversal(rootNode.leftNode);
+		System.out.print(rootNode.keyValue + " ");
+		inorderTraversal(rootNode.rightNode);
 	}
 
 	//function for PreOrder Traversal of the tree.
@@ -193,9 +192,9 @@ public class BinarySearchTree{
 			return;
 		}
 		// the formula for preorder is node, left, and right
-		System.out.print(rootNode.key + " " );
-		preorderTraversal(rootNode.left);
-		preorderTraversal(rootNode.right);
+		System.out.print(rootNode.keyValue + " " );
+		preorderTraversal(rootNode.leftNode);
+		preorderTraversal(rootNode.rightNode);
 	}
 
 	//function for  PostOrder Traversal of the Tree.
@@ -204,26 +203,26 @@ public class BinarySearchTree{
 			return;
 		}
 		// the formula for postorder is left, right and node
-		postorderTraversal(rootNode.left);
-		postorderTraversal(rootNode.right);
-		System.out.print(rootNode.key + " ");
+		postorderTraversal(rootNode.leftNode);
+		postorderTraversal(rootNode.rightNode);
+		System.out.print(rootNode.keyValue + " ");
 	}
 
 	//function to display the tree.
 	public static void display(){
 		System.out.println();
-		System.out.println("Inorder Traversal");
+		System.out.println("Inorder Traversal:");
 		inorderTraversal(root);
 
 		System.out.println();
-		System.out.println("\nPreOrder Traversal");
+		System.out.println("\nPreOrder Traversal:");
 		preorderTraversal(root);
 
 		System.out.println();
-		System.out.println("\nPostOrder Traversal");
+		System.out.println("\nPostOrder Traversal:");
 		postorderTraversal(root);
 
-		System.out.println();
+		System.out.println("\n");
 	}
 
 }
